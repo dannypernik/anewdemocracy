ac9184fab13b2ee3337c5185009c3c3999dd1d7e
Danny Pernik <danny@opencurrents.com>
2020-12-01 21:58:16 -0600

    replace ascended learning with a new democracy

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ascendedlearning
+# A New Democracy
--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 import os
-import sentry_sdk
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -9,13 +8,6 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_bootstrap import Bootstrap
 from flask_mailman import Mail
-from sentry_sdk.integrations.flask import FlaskIntegration
-
-sentry_sdk.init(
-    dsn="https://7b07a0f6a2244061a029e1deaa863161@o473156.ingest.sentry.io/5507799",
-    integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0
-)
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -54,4 +46,4 @@ if not app.debug:
         app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Ascended Learning')
+        app.logger.info('A New Democracy')
